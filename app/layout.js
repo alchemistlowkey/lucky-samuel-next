185e23279462257538d6ca99683d1abbd6110956
alchemistlowkey <alchemistlowkey@gmail.com>
2025-01-23 22:25:35 +0100

    robots and sitemap created

--- app/layout.js
+++ app/layout.js
@@ -28,8 +28,21 @@ export const metadata = {
         height: 600,
       },
     ],
-    locale: 'en_US',
-    type: 'website',
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
   },
 };
 
