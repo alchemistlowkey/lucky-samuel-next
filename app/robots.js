2b6d11ba35820e267be705daac897c23043c9560
alchemistlowkey <alchemistlowkey@gmail.com>
2025-01-23 23:51:18 +0100

    robots and sitemap updated

--- app/robots.js
+++ app/robots.js
@@ -5,6 +5,8 @@ export default function robots() {
       allow: "/",
       disallow: "/private/",
     },
+    sitemap: "https://luckysamuel.engineer/sitemap.xml",
     sitemap: "https://lucky-samuel-next.vercel.app/sitemap.xml",
+    sitemap: "https://luckysamuel.vercel.app/sitemap.xml",
   };
 }
